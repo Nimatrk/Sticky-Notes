5492a49df3f28859f29fb104ec4fb5ce6679d744
Nimatrk <nima.torkashvan5@gmail.com>
2024-12-05 14:20:57 +0330

    i wanted to use ddd in the app then i chosed not to

---
+++ Sticky Notes.Domain/Model/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sticky_Notes.Domain.Model
+{
+    internal class Note
+    {
+    }
+}
