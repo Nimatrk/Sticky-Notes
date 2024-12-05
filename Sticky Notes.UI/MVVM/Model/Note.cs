67704055c59ad928a81b812f49526a8803772236
Nimatrk <nima.torkashvan5@gmail.com>
2024-12-05 14:13:47 +0330

    defined a listviewitem for NotesListView

--- Sticky Notes.UI/MVVM/Model/Note.cs
+++ Sticky Notes.UI/MVVM/Model/Note.cs
@@ -4,24 +4,25 @@ namespace Sticky_Notes.UI.MVVM.Model;
 
 public class Note : ObservableObject, INote
 {
-	private DateTime _date;
+	private DateTime _creationDateTime = DateTime.Now;
 	private string? _text;
 	private IEnumerable<string> _images;
 
 	public DateTime CreationDateTime
     {
-		get { return _date; }
-		set { _date = value; OnPropertyChanged(); }
+		get { return _creationDateTime; }
+		set { _creationDateTime = value; OnPropertyChanged(); }
 	}
+
 	public string? Text
 	{
 		get { return _text; }
 		set { _text = value; OnPropertyChanged(); }
 	}
+
 	public IEnumerable<string> Images
 	{
 		get { return _images; }
 		set { _images = value; }
 	}
-
 }
